cb4f7c7c9b77f43fa57ebef88063f44e7df2df26
Siva Jagadeesan <sivajag@gmail.com>
2012-04-23 21:37:41 -0700

    Updated to clojure 1.4

--- project.clj
+++ project.clj
@@ -1,3 +1,38 @@
 (defproject zolo-utils "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "Clojure Utild"
+
+  :dependencies [[org.clojure/clojure "1.4.0"]
+
+                 [org.clojure/data.json "0.1.2"  :exclude [org.clojure/clojure]]
+                 [clj-http "0.3.6"]
+                 [clj-oauth2 "0.2.0"]
+
+                 [joda-time "1.6"]
+                 [clj-time "0.3.7"]
+                 [slingshot "0.10.2"]]
+
+  :plugins [[lein-swank "1.4.4"]
+            [lein-pprint "1.1.1"]
+            [lein-difftest "1.3.8"]
+            [lein-notes "0.0.1"]]
+
+  :hooks [leiningen.hooks.difftest]
+
+  :dev-dependencies [[clj-stacktrace "0.2.4"]
+                     [swank-clojure "1.3.3"]]
+  
+  :min-lein-version "1.7.0"
+
+  :test-selectors {:default (fn [t] (not (:integration t)))
+                   :integration :integration
+                   :all (fn [t] true)}
+  
+  :project-init (do (use 'clojure.pprint)
+                    (use 'clojure.test))
+
+  :warn-on-reflection false
+  
+  :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
+                 "local" ~(str (.toURI (java.io.File. "../mvn_repo")))}
+  
+  :resources-path "config")
