f9ff5e5f729d2037da393afd3d258eba7c449056
Amit Rathore <amitrathore@gmail.com>
2012-05-09 14:22:38 -0700

    added scaffold function to debug, to see what methods are on any given interface

--- src/zolodeck/utils/debug.clj
+++ src/zolodeck/utils/debug.clj
@@ -5,3 +5,17 @@
   (apply println (cons "*** " (map #(if (string? %) % (with-out-str (pprint %)))  args)))
   (last args))
 
+(defn scaffold
+  "Print the ancestor method signatures of a given interface."
+  [iface]
+  (doseq [[iface methods] (->> iface
+                               .getMethods
+                               (map #(vector (.getName (.getDeclaringClass %))
+                                             (symbol (.getName %))
+                                             (count (.getParameterTypes %))))
+                               (group-by first))]
+    (println (str "  " iface))
+    (doseq [[_ name argcount] methods]
+      (println
+       (str "    "
+            (list name (into ['this] (take argcount (repeatedly gensym)))))))))
