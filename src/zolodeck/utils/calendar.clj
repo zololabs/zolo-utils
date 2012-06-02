862a7ec86f874c402ff3c95e2e5ad9350525ad59
Amit Rathore <amitrathore@gmail.com>
2012-06-01 17:04:31 -0700

    added fn millis->instant

--- src/zolodeck/utils/calendar.clj
+++ src/zolodeck/utils/calendar.clj
@@ -5,6 +5,9 @@
 (defn date-string->instant [format date-string]
   (.toDate (parse (or (formatters format) (formatter format)) date-string)))
 
+(defn millis->instant [millis]
+  (java.sql.Timestamp. millis))
+
 (def BEGINNING-OF-TIME (date-time 1971 1 1))
 
 (def BEGINNING-OF-TIME-MILLIS (.getMillis BEGINNING-OF-TIME))
