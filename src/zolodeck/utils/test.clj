f7d1a88778c05b71af4ca4130d83acac552299dc
Siva Jagadeesan <sivajag@gmail.com>
2012-06-20 15:18:14 -0700

    Added deftest-pending and testing-pending

--- src/zolodeck/utils/test.clj
+++ src/zolodeck/utils/test.clj
@@ -3,3 +3,32 @@
 
 (defn is-same-sequence? [seq-a seq-b]
   (is (= (set seq-a) (set seq-b))))
+
+;; A Simple macro that enable to mark your test
+;; to pending
+(defn repeat-str- [n x]
+  (apply str (repeat n x)))
+
+(defn pending-str- [name]
+  (apply str
+         (repeat-str- 100 "*") "\n"
+         (repeat-str- 100 "*") "\n"
+         (repeat-str- 100 "*") "\n"
+         (repeat-str- 100 "*") "\n"
+         (repeat-str- 100 "*") 
+         "\n========\n" name " is pending !!\n========\n"
+         (repeat-str- 100 "*") "\n"
+         (repeat-str- 100 "*") "\n"
+         (repeat-str- 100 "*") "\n"
+         (repeat-str- 100 "*") "\n"
+         (repeat-str- 100 "*") "\n"))
+
+(defmacro deftest-pending [name & body]
+  (let [message (pending-str- name)]
+    `(deftest ~name
+       (println ~message))))
+
+(defmacro testing-pending [name & body]
+  (let [message (pending-str- name)]
+    `(testing ~name
+       (println ~message))))
