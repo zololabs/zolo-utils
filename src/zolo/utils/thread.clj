ac66ea923a9f4188df2cecfce364260127eb1b57
Amit Rathore <amitrathore@gmail.com>
2013-08-15 15:46:19 -0700

    fixed compile error in thread.clj, got slimed!

--- src/zolo/utils/thread.clj
+++ src/zolo/utils/thread.clj
@@ -10,25 +10,25 @@
 (defn- random-thread-name [prefix]
   (str prefix "-" (random-guid-str)))
 
-(defn- init-scheduled-executor []
-  (if (nil? @SCHEDULED-EXECUTOR)
-    (reset! SCHEDULED-EXECUTOR (scheduled-thread-pool-executor CPU-COUNT "ZOLO-UTILS-SCHEDULER"))))
-
 (defn thread-factory [thread-name-prefix]
   (proxy [ThreadFactory] []
     (newThread [thunk]
       (Thread. thunk (random-thread-name thread-name-prefix)))))
 
-(defn scheduled-thread-pool-executor [pool-size thread-name-prefix]
+(defn- scheduled-thread-pool-executor [pool-size thread-name-prefix]
   (->> thread-name-prefix
        thread-factory
        (ScheduledThreadPoolExecutor. pool-size)))
 
+(defn- init-scheduled-executor []
+  (if (nil? @SCHEDULED-EXECUTOR)
+    (reset! SCHEDULED-EXECUTOR (scheduled-thread-pool-executor CPU-COUNT "ZOLO-UTILS-SCHEDULER"))))
+
 (defn protected [thunk descriptor]
-  (try
-    (thunk)
-    (catch Exception e
-      (logger/error e (str "Error periodically executing " descriptor)))))
+  #(try
+     (thunk)
+     (catch Exception e
+       (logger/error e (str "Error periodically executing " descriptor)))))
 
 (defn run-thunk-periodically [descriptor thunk time-period-millis]
   (init-scheduled-executor)
