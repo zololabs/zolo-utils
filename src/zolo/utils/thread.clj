88b238c78b72bd8b17d33a209c2cb5b4e95efa3f
Amit Rathore <amitrathore@gmail.com>
2013-08-16 12:17:38 -0700

    refactored thread.clj to use defonce for initializing thread-pool

--- src/zolo/utils/thread.clj
+++ src/zolo/utils/thread.clj
@@ -3,10 +3,6 @@
   (:require [zolo.utils.logger :as logger])
   (:import [java.util.concurrent ThreadFactory ScheduledThreadPoolExecutor TimeUnit]))
 
-(def CPU-COUNT (.availableProcessors (Runtime/getRuntime)))
-
-(def SCHEDULED-EXECUTOR (atom nil))
-
 (defn- random-thread-name [prefix]
   (str prefix "-" (random-guid-str)))
 
@@ -15,22 +11,24 @@
     (newThread [thunk]
       (Thread. thunk (random-thread-name thread-name-prefix)))))
 
-(defn- scheduled-executor [pool-size thread-name-prefix]
+(defn scheduled-executor [pool-size thread-name-prefix]
   (->> thread-name-prefix
        thread-factory
        (ScheduledThreadPoolExecutor. pool-size)))
 
-(defn- init-scheduled-executor []
-  (if (nil? @SCHEDULED-EXECUTOR)
-    (reset! SCHEDULED-EXECUTOR (scheduled-executor CPU-COUNT "ZOLO-UTILS-SCHEDULER"))))
+(defonce SCHEDULED-EXECUTOR (scheduled-executor (.availableProcessors (Runtime/getRuntime))
+                                                "ZOLO-UTILS-SCHEDULER"))
 
-(defn protected [thunk descriptor]
+(defn- safe-fn [thunk descriptor]
   #(try
      (thunk)
      (catch Exception e
        (logger/error e (str "Error periodically executing " descriptor)))))
 
 (defn run-thunk-periodically [descriptor thunk time-period-millis]
-  (init-scheduled-executor)
-  (.scheduleAtFixedRate @SCHEDULED-EXECUTOR (protected thunk descriptor) time-period-millis time-period-millis TimeUnit/MILLISECONDS))
+  (.scheduleAtFixedRate SCHEDULED-EXECUTOR
+                        (safe-fn thunk descriptor)
+                        time-period-millis
+                        time-period-millis
+                        TimeUnit/MILLISECONDS))
 
